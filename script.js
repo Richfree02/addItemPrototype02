a47be6fb3cbe80a39ecbf8880051a51be0e4bc01
Richard Dominguez <richfree02@gmail.com>
2020-10-10 14:31:12 +0200

    The BUTTON now adds li experimental li elements to the experimental ul

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ console.log("I no suck anymorr :D");
 //button programing
 var getThat = document.getElementsByClassName('button');
 
+function addLi(name){
+  let li = document.createElement('li');
+  li.textContent = name;
+  return li;
+}
+
 for (var i = 0; i < getThat.length; i++){
   var button = getThat[i];
   button.addEventListener('click', function(){
     document.getElementById('prototypeButton').value = 'Does work!';
+    var list = document.getElementById('addMoreLi');
+    list.appendChild(addLi('New li hopeful-li'));
   }) 
 };
